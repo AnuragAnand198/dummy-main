2b604393cf8a4778b680d77749dbf29e69e367ef
AnuragAnand198 <anuraganand198@gmail.com>
2022-08-10 07:03:36 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+sys.setrecursionlimit(10**6)
+
+def make_commit(days: int):
+    if days < 902:
+        # Push
+        return os.system('git push')
+    else:
+        dates = f'{days} days ago'
+
+        with open('date.txt', 'a') as file:
+            file.write(f'{dates}\n')
+
+        # Staging
+        os.system("git add date.txt")
+
+        # Commit
+        os.system('git commit --date="'+dates+'" -m "First Commit"')
+              
+        return days * make_commit(days-1)
+        
+    
+
+make_commit(903) #2498
